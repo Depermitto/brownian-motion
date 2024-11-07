1db2a861b7518ed1afbf315f13b7df54214f264c
Depermitto <penciller@disroot.org>
2024-11-07 10:21:09 +0100

    add collision testing scene

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pygame
 import brownian
 
 
@@ -18,11 +19,27 @@ def get_scene2() -> brownian.Scene:
     return s
 
 
+def get_scene_collision_test(entities_n: int = 30) -> brownian.Scene:
+    maxx, maxy = pygame.display.get_window_size()
+
+    s = brownian.Scene("Collision test")
+    r1 = brownian.Entity((maxx / 2, maxy / 2), 75, (100, 200, 255))
+    s.register_entity(r1)
+
+    x = np.random.randint(0, maxx, entities_n)
+    y = np.random.randint(0, maxy, entities_n)
+    for i in range(entities_n):
+        s.register_entity(brownian.Entity((x[i], y[i]), 50))
+
+    return s
+
+
 def main():
     print("Hello, FO!")
     app = brownian.App()
     app.register_scene(get_scene1())
     app.register_scene(get_scene2())
+    app.register_scene(get_scene_collision_test())
     app.run()
     print("Goodbye!")
 
